9f2b4131ab84f56814d701e135608c2fa1a954be
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-09-16 12:06:00 -0400

    Update User.java

--- src/project/annotations/User.java
+++ src/project/annotations/User.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class User {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);		
-		System.out.println();
+		System.out.println(input);
+		
 	}
 }
