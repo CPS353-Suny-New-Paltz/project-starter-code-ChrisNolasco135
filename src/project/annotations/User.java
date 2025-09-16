3a006e43ebf6382b99b627b3362988f382baad6c
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-09-16 11:14:57 -0400

    Added Components
    Added the components for the API to interact with

---
+++ src/project/annotations/User.java
@@ -0,0 +1,10 @@
+package project.annotations;
+
+import java.util.Scanner;
+
+public class User {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);		
+		System.out.println();
+	}
+}
