f7eda198f7e929189876eed4a7f4cbc4147b62d9
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-09-16 15:06:45 -0400

    Added NetworkAPI Draft
    This API allows for the user to specify a source, a destination and a delimiter for the input they will give and send it to their desired destination.

--- src/project/annotations/NetworkAPIPrototype.java
+++ src/project/annotations/NetworkAPIPrototype.java
@@ -5,8 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@NetworkAPIPrototype
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NetworkAPIPrototype {
-	// Marker annotation, should be applied to a method within a prototype class
+	public interface NetworkAPI{
+		void setInputSource(String sourcePath);
+		void setDelimiter(String delimeter);
+		void setOutputDestination(String destinationPath);
+		void process() throws Exception;
+	}
+	public interface DataSource{
+		String readData() throws Exception;
+	}
+	public interface DataDestination{
+		void writeData(String data) throws Exception;
+	}
+	
 }
