a63324fad3aa0ee0f85cd7840ba3a26beea82057
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-10-22 14:54:45 -0400

    Updated readData parameter

--- test/junittests/java/storage/TestStorageComputeAPI.java
+++ test/junittests/java/storage/TestStorageComputeAPI.java
@@ -21,7 +21,7 @@ class TestStorageComputeAPI {
         ComputationAPI mockComp = Mockito.mock(ComputationAPI.class);
         StorageComputeImpl storage = new StorageComputeImpl(mockComp);
 
-        List<Integer> data = storage.readData();
+        List<Integer> data = storage.readData(DataBatch.EMPTY);
         assertTrue(data.isEmpty(), "Default readData should return empty list");
     }
 
