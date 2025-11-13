f25867eca8ae6801b02bb16396c9b2643b378f9c
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-11-13 16:36:07 -0500

    Added Tests
    *Added new test in ComputeEngineIntegrationTest named testEndToEndWithEmptyInput which will test what happens if the input is empty.
    
    *Added Checkpoint 4 Tests
    
    *Added the implementations of all 3 APIs and added the computation test in the ManualTestingFramework

---
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -0,0 +1,41 @@
+package project.checkpointtests;
+
+import compute.ComputationImpl;
+import storage.StorageComputeImpl;
+import user.UserComputeImpl;
+import user.DataSource;
+import user.DataDestination;
+
+public class ManualTestingFramework {
+    public static final String INPUT = "manualTestInput.txt";
+    public static final String OUTPUT = "manualTestOutput.txt";
+
+    public static void main(String[] args) {
+        // TODO 1:
+        // Instantiate a real (ie, class definition lives in the src/ folder) implementation 
+        // of all 3 APIs
+        ComputationImpl computationAPI = new ComputationImpl();
+        StorageComputeImpl storageAPI = new StorageComputeImpl(computationAPI);
+        UserComputeImpl userAPI = new UserComputeImpl(storageAPI, computationAPI);
+
+        // TODO 2:
+        // Run a computation with an input file of <root project dir>/manualTestInput.txt
+        // and an output of <root project dir>/manualTestOutput.txt, with a delimiter of ',' 
+        DataSource inputSource = new DataSource() {
+			@Override
+			public String getIdentifier() {
+				return INPUT;
+			}
+		};
+        DataDestination outputDestination = new DataDestination() {
+        	@Override
+        	public String getIdentifier() {
+        		return OUTPUT;
+        	}
+        };
+        String delimiter = ",";
+
+        boolean success = userAPI.submitJob(inputSource, outputDestination, delimiter);
+        System.out.println("Computation success: " + success);
+    }
+}
