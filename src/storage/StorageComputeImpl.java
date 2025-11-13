fda8a361ee114b9b0dfde4e7b306b801ca5e96ea
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-11-13 16:28:05 -0500

    Implementation of Compute Engine
    *Added the computation component which will read the the input and run it through the computation to then return a string of letters translated from the Caesar Cipher.
    
    *The coordination component will take in an input source, output source and potential delimiter to submit a job to request.
    
    *Updated the data storage component to read and write data from the other components

--- src/storage/StorageComputeImpl.java
+++ src/storage/StorageComputeImpl.java
@@ -1,22 +1,75 @@
 package storage;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import user.DataSource;
+import user.DataDestination;
 
 public class StorageComputeImpl implements StorageComputeAPI {
+    private DataSource source;
+    private DataDestination destination;
+    private String delimiter = ",";
+
     public StorageComputeImpl(compute.ComputationAPI computationAPI) {
+        // Optionally store computationAPI if needed
+    }
+
+    private String getFilePath(DataSource data) {
+        if (data instanceof user.DataSource) {
+            return ((user.DataSource) data).getIdentifier();
+        }
+        return null;
     }
 
     @Override
-    public List<Integer> readData(DataBatch data) {
-        // Stub: return empty list
-        return new ArrayList<>();
+    public List<Integer> readData(DataSource data) {
+        String filePath = getFilePath(data);
+        List<Integer> result = new ArrayList<>();
+        if (filePath == null) return result;
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (String part : line.split("[ ,;\t]+")) {
+                    try {
+                        result.add(Integer.parseInt(part.trim()));
+                    } catch (NumberFormatException ignored) {}
+                }
+            }
+        } catch (IOException e) {
+        	
+        }
+        return result;
     }
 
     @Override
     public boolean writeData(List<Integer> data) {
-        // Stub: pretend to fail
-        return false;
+        if (destination == null) return false;
+        String filePath = destination.getIdentifier();
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath))) {
+            for (Integer i : data) {
+                writer.write(i.toString());
+                writer.write(" ");
+            }
+            writer.newLine();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 
+    // Optionally add methods to set source/destination
+    public void setSource(DataSource source) {
+        this.source = source;
+    }
+    public void setDestination(DataDestination destination) {
+        this.destination = destination;
+    }
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
 }
