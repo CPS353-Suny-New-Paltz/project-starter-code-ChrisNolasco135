1ca3904114c595fdf1be3687e853054d204ded56
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-12-04 15:34:29 -0500

    Fixed the TODO list
    Added the specifications asked by the todo list stated in the testHarness folder, which includes:
    *Changing the variable type to the name I've used for this project
    *Created an instance of the implementation of my @NetworkAPI and added it to the coordinator instance variable.
    *Changed the type of variables in the TestUser.java file
    *Called the appropriate methods on the coordinator to call the computeJob method

---
+++ test/testHarness/TestUser.java
@@ -0,0 +1,27 @@
+package testHarness;
+
+import java.io.File;
+import user.UserComputeAPI;
+
+
+public class TestUser {
+	
+	// TODO 3: change the type of this variable to the name you're using for your
+	// @NetworkAPI interface; also update the parameter passed to the constructor
+	private final user.UserComputeAPI coordinator;
+
+	public TestUser(user.UserComputeAPI coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		char delimiter = ';';
+		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// run the compute job specified by inputPath, outputPath, and delimiter
+		user.DataSource inputSource = () -> inputPath;
+		user.DataDestination outputDestination = () -> outputPath;
+		coordinator.submitJob(inputSource, outputDestination, String.valueOf(delimiter));
+	}
+
+}
