fdcc5ce96447efcb77b0e46fc5522c5010b36ed1
ChrisNolasco135 <chrisnolasco135@gmail.com>
2025-12-04 20:21:40 -0500

    Added MultiThreadedUserComputeImpl
    *Added the  MultiThreadedUserComputeImpl file which will partition the job submission under a maximum amount of threads which is listed in the README file

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Software Engineering Project Starter Code
 
-This repo will start you off with an initial configuration that you'll modify as part of Checkpoint 1. As part of the modifications, you'll eventually delete the contents of this README and replace it with documentation for your project.
+This repository will start you off with an initial configuration that you'll modify as part of Checkpoint 1. As part of the modifications, you'll eventually delete the contents of this README and replace it with documentation for your project.
 
 Checkpoint 2
-This system will recieve an numerical input and decode the input utilizing Caesar's Cipher with a shift of 7, if given an input of 0124050508001608110523, it will result in Hello World 
+This system will receive an numerical input and decode the input utilizing Caesar's Cipher with a shift of 7, if given an input of 0124050508001608110523, it will result in Hello World 
+
+Checkpoint 6
+The upper bound for the number of threads to be utilized is a maximum of 4 threads. This is to ensure that the system does not become overloaded with too many threads, which could lead to performance degradation. The system will dynamically allocate threads based on the workload, but will never exceed the maximum limit of 4 threads.
 
 ![Image of System Diagram](https://github.com/CPS353-Suny-New-Paltz/project-starter-code-ChrisNolasco135/blob/main/images/SystemDiagram.jpg?raw=true)
--- src/user/MultiThreadedUserComputeImpl.java
+++ src/user/MultiThreadedUserComputeImpl.java
@@ -1,5 +1,90 @@
 package user;
 
-public class MultiThreadedUserComputeImpl {
+import java.util.ArrayList;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+
+public class MultiThreadedUserComputeImpl implements UserComputeAPI {
+    private static final int MAX_THREADS = 4; 
+    private storage.StorageComputeAPI storageAPI;
+    private compute.ComputationAPI computeAPI;
+    private final ExecutorService executor;
+
+    public MultiThreadedUserComputeImpl(storage.StorageComputeAPI storageAPI, compute.ComputationAPI computeAPI) {
+        this.storageAPI = storageAPI;
+        this.computeAPI = computeAPI;
+        this.executor=Executors.newFixedThreadPool(MAX_THREADS);
+    }
+
+    @Override
+    public boolean submitJob(DataSource source, DataDestination destination, String delimiter) {
+        setInputSource(source);
+        setOutputDestination(destination);
+        setDelimiters(delimiter);
+        List<Integer> inputData = storageAPI.readData(source);
+        if (inputData == null || inputData.isEmpty()) {
+            return false;
+        }
+        int partitionSize = (int) Math.ceil(inputData.size() / (double) MAX_THREADS);
+        List<Future<List<Integer>>> futures = new ArrayList<>();
+        ExecutorService executor = Executors.newFixedThreadPool(MAX_THREADS);
+        for (int i = 0; i < inputData.size(); i += partitionSize) {
+            int start = i;
+            int end = Math.min(i + partitionSize, inputData.size());
+            List<Integer> partition = inputData.subList(start, end);
+            futures.add(executor.submit(() -> computeAPI.processJob(partition)));
+        }
+        List<Integer> combinedResults = new ArrayList<>();
+        try {
+            for (Future<List<Integer>> future : futures) {
+                List<Integer> result = future.get();
+                if (result != null) {
+                    combinedResults.addAll(result);
+                }
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            executor.shutdown();
+            return false;
+        }
+        executor.shutdown();
+        boolean writeSuccess = storageAPI.writeData(combinedResults);
+        return writeSuccess;
+    }
+
+    @Override
+    public List<String> getResults() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public DataSource setInputSource(DataSource source) {
+        return source;
+    }
+
+    @Override
+    public DataDestination setOutputDestination(DataDestination destination) {
+        return destination;
+    }
+
+    @Override
+    public String setDelimiters(String delimiter) {
+        return delimiter;
+    }
+
+    @Override
+    public DataSource executeJob(DataSource source) {
+        return null;
+    }
+
+	public void shutdown() {
+		executor.shutdown();
+		
+	}
 }
